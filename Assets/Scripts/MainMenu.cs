0e6b30aebe6abdd95fce740b321fff8cc326c240
Enesakarr50 <enesakar205@gmail.com>
2024-06-28 17:36:16 +0300

    4
    4 anim finito

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        // Ýlk sahneyi yükler. Sahne ismini deðiþtirebilirsiniz.
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void QuitGame()
+    {
+        // Oyun derleme sýrasýnda çalýþýr. Editor modunda çalýþmaz.
+        Application.Quit();
+
+        // Editor modunda çalýþmayý saðlamak için:
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
