39752139adc401ac0d6a73222cc8cbee9f618648
Enesakarr50 <enesakar205@gmail.com>
2024-07-01 17:49:49 +0300

    asd
    asda

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     public Transform attackPoint;
-    public int attackRange;
+    public float attackRange;
     public LayerMask enemyLayers;
     public int attackDamage = 10;
     public ClassTypes Class;
@@ -17,9 +17,6 @@ public class PlayerAttack : MonoBehaviour
     private void Start()
     {
         mainCamera = Camera.main;
-        
-        
-
     }
 
     void Update()
@@ -27,41 +24,36 @@ public class PlayerAttack : MonoBehaviour
         if (Class == null)
         {
             Class = GameManager.CurrentClass;
-
-        }
-        else if (Class == GameManager.Mage)
-        {
-            attackRange = 10;
-        }
-        else if (Class == GameManager.Knight)
-        {
-            attackRange = 1;
-        }
-        else if (Class == GameManager.Hunter)
-        {
-            attackRange = 1;
         }
 
-        
+        if (Class == GameManager.Mage)
+        {
+            attackRange = 10f;
+        }
+        else
+        {
+            attackRange = 1f;
+        }
+
         if (Input.GetMouseButtonDown(0))
         {
             Attack();
-            Shoot();
 
+            if (Class == GameManager.Mage)
+            {
+                Shoot();
+            }
         }
     }
 
     void Attack()
     {
-        
         Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-        mousePosition.z = 0f; 
+        mousePosition.z = 0f;
 
-        
         Vector3 attackDirection = (mousePosition - attackPoint.position).normalized;
         Debug.DrawRay(attackPoint.position, attackDirection * attackRange, Color.red, 0.1f);
 
-        
         RaycastHit2D[] hitEnemies = Physics2D.RaycastAll(attackPoint.position, attackDirection, attackRange, enemyLayers);
 
         foreach (RaycastHit2D hit in hitEnemies)
@@ -72,13 +64,18 @@ public class PlayerAttack : MonoBehaviour
 
     void Shoot()
     {
-        GameObject fireball = Instantiate(fireballPrefab, firePoint.position, firePoint.rotation); // Fireball oluþtur
+        Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0f;
+
+        Vector3 shootDirection = (mousePosition - firePoint.position).normalized;
+        Quaternion rotation = Quaternion.LookRotation(Vector3.forward, shootDirection);
+
+        GameObject fireball = Instantiate(fireballPrefab, firePoint.position, rotation);
         Rigidbody2D rb = fireball.GetComponent<Rigidbody2D>();
+
         if (rb != null)
         {
-            rb.velocity = firePoint.right * fireballSpeed; // Fireball'a hýz ver
+            rb.velocity = shootDirection * fireballSpeed;
         }
     }
-
-
 }
